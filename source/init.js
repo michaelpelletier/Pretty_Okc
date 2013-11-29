d9121294679667c4ceaaaaaa5a8f81df07f675c6
Michael Pelletier <admin@mpelletier.net>
2013-11-28 20:11:27 -0500

    Testing background stripping idea.

--- source/init.js
+++ source/init.js
@@ -1,1 +1,16 @@
 document.getElementsByTagName('body')[0].className += " pretty_okc";
+
+handleChanges = function() {
+	$('.match_card_wrapper').each(function() {
+		var match = $(this).find('.percentages').text();
+		match = match.split('%');
+		$(this).find('.percentages').text(match[0]);
+	});
+}
+
+handleChanges();
+
+var observer = new MutationSummary({
+  callback: handleChanges,
+  queries: [{ element: '.match_card_wrapper' }]
+});
