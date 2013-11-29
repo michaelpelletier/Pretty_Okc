d35142003c973c33c4525c2d3b594b105810766d
Michael Pelletier <admin@mpelletier.net>
2013-11-28 23:36:29 -0500

    Add comments and fix jQuery function.

--- source/init.js
+++ source/init.js
@@ -1,20 +1,22 @@
-document.getElementsByTagName('body')[0].className += " pretty_okc";
+$('body').addClass("pretty_okc");
 
 handleChanges = function() {
 	$('.match_card_wrapper').each(function() {
+		// Remove "Match" from Match Percentage.
 		var match = $(this).find('.percentages').text();
-		match = match.split('%');
+		match = match.split(' ');
 		$(this).find('.percentages').text(match[0]);
 
+		// Remove state abbreviation.
 		var town = $(this).find('.userinfo').text();
 		town = town.replace('·', ' · ').split(',');
-		$(this).find('.userinfo').text(town[0]);
-
+		$(this).find('.userinfo').text(town[0]);	
 	});
 }
 
 handleChanges();
 
+// When more users are added to the page, call the function.
 var observer = new MutationSummary({
   callback: handleChanges,
   queries: [{ element: '.match_card_wrapper' }]
