1488613f958ffdc6390134608e913eb95e464b10
Michael Pelletier <admin@mpelletier.net>
2013-12-08 13:22:36 -0500

    Fix path names and rename functions. Also return settings when asked.

--- source/background.js
+++ source/background.js
@@ -1,16 +1,24 @@
-chrome.tabs.onCreated.addListener(do_something);
-chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
-    if (info.status == 'complete') do_something();
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  var my_settings = localStorage["mode"];
+
+  if (request.retrieve == "settings"){
+    sendResponse({settings: my_settings});
+  }
 });
 
-function do_something() {
+chrome.tabs.onCreated.addListener(inject_css);
+chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
+    if (info.status == 'complete') inject_css();
+});
+
+function inject_css() {
   var my_settings = localStorage["mode"];
   var path;
 
   if (my_settings === "classic") {
-  	path = "classic.css"
+  	path = "css/classic.css"
   } else {
-  	path = "tiles.css"
+  	path = "css/tiles.css"
   }
 
 	chrome.tabs.insertCSS(null, {file: path});
