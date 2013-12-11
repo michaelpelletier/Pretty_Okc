070d52e6ddcf88bcc56d31529082fd544ec92f86
Michael Pelletier <admin@mpelletier.net>
2013-12-11 17:12:31 -0500

    Add Add_Notes Button

--- source/init.js
+++ source/init.js
@@ -1,5 +1,11 @@
 $('body').addClass("pretty_okc");
 
+
+fix_private_notes = function() {
+	var onclick = "Profile.loadWindow('edit_notes', 244); return false;"
+	$('.action_options').append('<p class="btn small white"><a onclick="' + onclick + '">Add Note</a></p>');
+}
+
 handleChanges = function() {
 	$('.match_card_wrapper').each(function() {
 		var self = $(this);
@@ -19,6 +25,7 @@ handleChanges = function() {
 }
 
 handleChanges();
+fix_private_notes();
 
 // When more users are added to the page, call the function.
 var observer = new MutationSummary({
