b2fbd78d494278a58c6138321de7c30188f6a0af
Michael Pelletier <admin@mpelletier.net>
2013-12-11 20:05:33 -0500

    Continue adding functionality to options page.

--- source/js/options.js
+++ source/js/options.js
@@ -23,8 +23,16 @@ function save_options() {
     var id = $(this).attr('id');
     priority_array.push(id);
   });
-
   localStorage["priority"] = priority_array;
+
+  // Store "Add Notes" Setting
+  var add_notes;
+  if ($("#add_notes").is(':checked')) {
+    add_notes = true;
+  } else {
+    add_notes = false;
+  }
+  localStorage["add_notes"] = add_notes;
 
   // Update status to let user know options were saved.
   var status = $("#status");
@@ -51,4 +59,11 @@ function restore_options() {
     var item = $('li#' + priority_settings[i]);
     $('#excerpt_priority').append(item);
   }
+
+  // Load Add_Notes Settings
+  var add_notes = localStorage["add_notes"];
+  if (add_notes === "true") {
+    $('#add_notes').attr('checked', true);
+  }
+
 }
